6dcdb25d0c8439b29976bfd31f80bf79b6fa974e
Saadat Badgujar <saadatbadgujar@gmail.com>
2021-11-19 08:56:01 +0530

    Made a basic HTML layout and a function to populate the display

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const zero = document.querySelector('#zero');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const screen = document.querySelector('#screen');
+
+let text = '';
+
+function display(e){
+    let input = e.target.textContent;
+    text = text+input;
+    console.log(text);
+    screen.textContent = text;
+};
+
+zero.addEventListener('click', display)
+one.addEventListener('click', display)
+two.addEventListener('click', display)
+three.addEventListener('click', display)
+four.addEventListener('click', display)
+five.addEventListener('click', display)
+six.addEventListener('click', display)
+seven.addEventListener('click', display)
+eight.addEventListener('click', display)
+nine.addEventListener('click', display)
+
+
 // ADD FUNCTION
 function add(a,b){
     return a+b;
